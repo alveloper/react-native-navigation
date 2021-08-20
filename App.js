c7e17cfa3c19cd19913660f1952fe412f83830dd
Suyeon <alveloper@gmail.com>
2021-08-20 15:52:54 +0900

    Navigating to a new screen

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-function HomeScreen() {
+function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Home Screen</Text>
+      {/*
+      (5) navigation - the navigation prop is passed in to every screen component in the native stack navigator.
+      navigate('Details') - we call the navigate function with the name of the route that we'd like to move the user to.
+      */}
+      <Button
+        title="Go to Details" 
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 }
