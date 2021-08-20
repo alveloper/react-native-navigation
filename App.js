8cecc1706dacbd348249134625e8902c19b45f65
Suyeon <alveloper@gmail.com>
2021-08-20 15:39:49 +0900

    Creating a native stack navigator

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+/*
+(1) createNativeStackNavigator is a function that returns an object containing 2 properties: Screen and Navigator.
+The Navigator should contain Screen elements as its children to define the configuration for routes.
+*/
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    /*
+    (2) NavigationContainer is a component which manages our navigation tree.
+    This component must wrap all navigators structure.
+    */
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
