2322f85a8080aba35c16bda3262b7b38494b0825
Suyeon <alveloper@gmail.com>
2021-08-20 15:45:07 +0900

    Configuring the navigator

--- App.js
+++ App.js
@@ -11,6 +11,14 @@ function HomeScreen() {
   );
 }
 
+function DetailScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+    </View>
+  )
+}
+
 /*
 (1) createNativeStackNavigator is a function that returns an object containing 2 properties: Screen and Navigator.
 The Navigator should contain Screen elements as its children to define the configuration for routes.
@@ -26,6 +34,13 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
+        {/*
+        (3) Now our stack has two routes, a Home route and a Details route. 
+        A route can be specified by using the Screen component. 
+        The Screen component accepts a name prop which corresponds to the name of the route we will use to navigate,
+         and a component prop which corresponds to the component it'll render.
+        */}
+        <Stack.Screen name="Details" component={DetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
