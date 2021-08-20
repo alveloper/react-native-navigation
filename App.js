7f16553cfba62cfdb4ee0585739b541bf782e890
Suyeon <alveloper@gmail.com>
2021-08-20 15:47:57 +0900

    Specifying options

--- App.js
+++ App.js
@@ -33,7 +33,12 @@ function App() {
     */
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        {/*
+        (4)  Each screen in the navigator can specify some options for the navigator,
+        such as the title to render in the header.
+        These options can be passed in the options prop for each screen component.
+         */}
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Overview' }} />
         {/*
         (3) Now our stack has two routes, a Home route and a Details route. 
         A route can be specified by using the Screen component. 
